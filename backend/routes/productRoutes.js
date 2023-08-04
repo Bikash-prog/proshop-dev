2e56da97020b6c323fa0f51146dab7606005e099
Bikash Bhuyan <bikashbhuyan802@gmail.com>
2023-08-05 01:24:49 +0530

    added file upload,products review and upsert and deletion functionatily

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,23 @@ import express from "express";
 import {
   getProducts,
   getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview,
 } from "../controllers/productController.js";
+
+import { protect, admin } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 // create a route to get all products
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 // create a route to get a single product
-router.route("/:id").get(getProductById);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
