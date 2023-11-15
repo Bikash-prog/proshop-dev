23c1118f983f353a064d32a226b5966586c5ad6d
Bikash Bhuyan <bikashbhuyan802@gmail.com>
2023-11-15 13:06:58 +0530

    final push

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
 //-------------------------------------------//
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
 // routes for product APIs
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -37,6 +34,18 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 // middleware for error handling
 app.use(notFound);
 app.use(errorHandler);
