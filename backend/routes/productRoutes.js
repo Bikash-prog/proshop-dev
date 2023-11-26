acaeeca22bec3436edb0b9c14d5a56bb394c0c31
Bikash Bhuyan <bikashbhuyan802@gmail.com>
2023-11-27 01:58:24 +0530

    changed for feature testing

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -21,6 +21,7 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+
 router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,14 +2,14 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "3d",
+    expiresIn: "1d",
   });
   // set JWT as http only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
-    maxAge: 1000 * 60 * 60 * 24 * 3, // 3 days
+    maxAge: 1000 * 60 * 60 * 24 * 1, // 1 days
   });
 };
 
